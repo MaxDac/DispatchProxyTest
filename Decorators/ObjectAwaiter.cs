00cb5c9d47ee1b7d6a9aecf9a45d643b451f96d1
Massimiliano D'Acunzo <mdacunzo@microsoft.com>
2023-01-27 12:19:52 +0000

    Trying to catch task exceptions

---
+++ Decorators/ObjectAwaiter.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ConsoleApp6.Decorators
+{
+	internal class ObjectAwaiter
+	{
+		private readonly object? thisIsATaskISwear;
+		private readonly Type genericTaskType;
+
+		public ObjectAwaiter(object? thisIsATaskISwear, Type genericTaskType)
+		{
+			this.thisIsATaskISwear = thisIsATaskISwear;
+			this.genericTaskType = genericTaskType;
+		}
+
+		public TaskAwaiter GetAwaiter()
+		{
+			var getAwaiterMethod = this.genericTaskType.GetMethod(nameof(GetAwaiter));
+			var uncastedResult = getAwaiterMethod?.Invoke(this.thisIsATaskISwear, null);
+
+			if (uncastedResult == null)
+			{
+				throw new InvalidOperationException("The awaiter is null");
+			}
+
+			return (TaskAwaiter)uncastedResult;
+		}
+	}
+}
--- Decorators/TryCatchDecorator.cs
+++ Decorators/TryCatchDecorator.cs
@@ -2,35 +2,95 @@
 
 namespace ConsoleApp6.Decorators
 {
-    public class TryCatchDecorator<TDecorated> : DispatchProxy
-        where TDecorated : class
-    {
-        private TDecorated? decorated;
+	public class TryCatchDecorator<TDecorated> : DispatchProxy
+		where TDecorated : class
+	{
+		private TDecorated? decorated;
 
-        protected override object? Invoke(MethodInfo? targetMethod, object?[]? args)
-        {
-            // Try catching
-            try
-            {
-                return targetMethod?.Invoke(decorated, args);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Decorator caught the exception");
-                Console.WriteLine(ex.ToString());
-                return null;
-            }
-        }
+		protected override object? Invoke(MethodInfo? targetMethod, object?[]? args)
+		{
+			// Try catching
+			var isTask = IsTask(targetMethod?.ReturnType);
+			var isGeneric = targetMethod?.ReturnType?.IsGenericType == true;
 
-        public static TDecorated Create(TDecorated decorated)
-        {
-            object proxy = Create<TDecorated, TryCatchDecorator<TDecorated>>();
-            ((TryCatchDecorator<TDecorated>)proxy).SetParameter(decorated);
+			if (isTask)
+			{
+				// Generic Task, means that it returns a value
+				if (isGeneric)
+				{
+					var genericTaskType = targetMethod?.ReturnType?.GetGenericArguments()[0];
 
-            return (TDecorated)proxy;
-        }
+					if (genericTaskType == null)
+					{
+						return null;
+					}
 
-        private void SetParameter(TDecorated decorated) =>
-            this.decorated = decorated ?? throw new ArgumentNullException(nameof(decorated));
-    }
+					var lambda = async () =>
+					{
+						try
+						{
+							var result = targetMethod?.Invoke(decorated, args);
+							return await new ObjectAwaiter(result, targetMethod?.ReturnType);
+						}
+						catch (Exception ex)
+						{
+							Console.WriteLine("Decorator caught the exception");
+							Console.WriteLine(ex.ToString());
+							return null;
+						}
+					};
+
+					var method = typeof(Task).MakeGenericType(genericTaskType).GetMethod(nameof(Task.Run))?.MakeGenericMethod(genericTaskType);
+					return method?.Invoke(null, new[] { lambda });
+				}
+				else
+				{
+					return Task.Run(async () =>
+					{
+						try
+						{
+							if (targetMethod?.Invoke(decorated, args) is not Task task)
+							{
+								throw new InvalidOperationException("Error while getting the task result");
+							}
+
+							await task;
+						}
+						catch (Exception ex)
+						{
+							Console.WriteLine("Decorator caught the exception");
+							Console.WriteLine(ex.ToString());
+						}
+					}).GetAwaiter();
+				}
+			}
+			else
+			{
+				try
+				{
+					return targetMethod?.Invoke(decorated, args);
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("Decorator caught the exception");
+					Console.WriteLine(ex.ToString());
+					return null;
+				}
+			}
+		}
+
+		public static TDecorated Create(TDecorated decorated)
+		{
+			object proxy = Create<TDecorated, TryCatchDecorator<TDecorated>>();
+			((TryCatchDecorator<TDecorated>)proxy).SetParameter(decorated);
+
+			return (TDecorated)proxy;
+		}
+
+		private void SetParameter(TDecorated decorated) =>
+			this.decorated = decorated ?? throw new ArgumentNullException(nameof(decorated));
+
+		private static bool IsTask(Type? type) => type?.Name == nameof(Task);
+	}
 }
+
